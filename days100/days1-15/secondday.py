743e6fc54ea727cb08093ae28ef804d903b38fde
JasonDurian <824676495@qq.com>
2021-01-18 18:03:42 +0800

    the second day of python-daily

---
+++ days100/days1-15/secondday.py
@@ -0,0 +1,10 @@
+a = int(input('a = '))
+b = int(input('b = '))
+c = 1
+c += a
+c += b
+
+print('(%d) + (%d) = %d' % (a, b, c))
+print('(%d) // (%d) = %f' % (a, b, a // b))
+print('(%d) %% (%d) = %d' % (a, b, a % b))
+print('(%d) ** (%d) = %d' % (a, b, a ** b))
