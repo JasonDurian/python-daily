a9b62c7001637927ba4487a4b50b27e6f564ed9d
JasonDurian <824676495@qq.com>
2021-01-18 14:53:44 +0800

    the first day of python-daily

---
+++ days100/days1-15/firstday.py
@@ -0,0 +1,19 @@
+"""
+The first python program
+"""
+
+import turtle
+
+turtle.pensize(5)
+turtle.pencolor('yellow')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
+
