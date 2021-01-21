87974527520f5061295fccabccb2a690f540ee4c
jasonvan <jasondurianvan@gmail.com>
2021-01-21 23:24:58 +0800

    the fifth day of python-daily

--- days100/days1-15/fourthday.py
+++ days100/days1-15/fourthday.py
@@ -12,18 +12,31 @@ Calculate the sum from 1 to 100
 """
 Number guessing game
 """
-answer = random.randint(1, 100)
-counter = 0
-while True:
-    counter += 1
-    number = int(input('Please input your number: '))
-    if number < answer:
-        print('Bigger')
-    elif number > answer:
-        print('Smaller')
-    else:
-        print('Congratulations on your right answer!')
+# answer = random.randint(1, 100)
+# counter = 0
+# while True:
+#     counter += 1
+#     number = int(input('Please input your number: '))
+#     if number < answer:
+#         print('Bigger')
+#     elif number > answer:
+#         print('Smaller')
+#     else:
+#         print('Congratulations on your right answer!')
+#         break
+# print('You made %d guesses in total' % counter)
+# if counter > 7:
+#     print('Your IQ balance is obviously inadequate!')
+
+"""
+Calculate the greatest common divisor and the least common multiple
+"""
+x = int(input('x = '))
+y = int(input('y = '))
+if x > y:
+    x, y = y, x
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('The greater common divisor of %d and %d is %d' % (x, y, factor))
+        print('The least common multiple of %d and %d is %d' % (x, y, x * y // factor))
         break
-print('You made %d guesses in total' % counter)
-if counter > 7:
-    print('Your IQ balance is obviously inadequate!')
