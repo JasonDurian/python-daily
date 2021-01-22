57ff72ac1f0bd26766ed82b2c4e3f1aa9e430d42
jasonvan <jasondurianvan@gmail.com>
2021-01-22 17:29:43 +0800

    the fifth day of python-daily

--- days100/days1-15/fifthday.py
+++ days100/days1-15/fifthday.py
@@ -2,9 +2,19 @@
 """
 Find the number of daffodils
 """
-for num in range(100, 1000):
-    low = num % 10
-    mid = num // 10 % 10
-    high = num // 100
-    if num == low ** 3 + mid ** 3 + high ** 3:
-        print(num)
+# for num in range(100, 1000):
+#     low = num % 10
+#     mid = num // 10 % 10
+#     high = num // 100
+#     if num == low ** 3 + mid ** 3 + high ** 3:
+#         print(num)
+
+"""
+Inversion of a positive integer
+"""
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
