75c0eb04dcb7e5511218cd53d368e34747d095f7
jasonvan <jasondurianvan@gmail.com>
2021-01-27 23:39:52 +0800

    the 6th day of python-daily

--- days100/days1-15/day06/test.py
+++ days100/days1-15/day06/test.py
@@ -1,6 +1,11 @@
 import module1 as m1
 import module2 as m2
 import module3
+import divisor_multiple as dm
 
-m1.foo()
-m2.foo()
+# m1.foo()
+# m2.foo()
+a = int(input('Please input x: '))
+b = int(input('Please input y: '))
+print(dm.gcd(a, b))
+print(dm.lcm(a, b))
