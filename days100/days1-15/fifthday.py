b61a722f179a0df8aa066bd1099556620a44cdf7
jasonvan <jasondurianvan@gmail.com>
2021-01-27 14:56:47 +0800

    the fifth day of python-daily

--- days100/days1-15/fifthday.py
+++ days100/days1-15/fifthday.py
@@ -1,4 +1,3 @@
-
 """
 Find the number of daffodils
 """
@@ -12,9 +11,20 @@ Find the number of daffodils
 """
 Inversion of a positive integer
 """
-num = int(input('num = '))
-reversed_num = 0
-while num > 0:
-    reversed_num = reversed_num * 10 + num % 10
-    num //= 10
-print(reversed_num)
+# num = int(input('num = '))
+# reversed_num = 0
+# while num > 0:
+#     reversed_num = reversed_num * 10 + num % 10
+#     num //= 10
+# print(reversed_num)
+
+"""
+Hundreds of money and hundreds of chickens
+"""
+# for x in range(0, 21):
+for x in range(0, 20):
+    # for y in range(0, 34):
+    for y in range(0, 33):
+        z = 100 - x - y
+        if 5 * x + 3 * y + z / 3 == 100:
+            print('Cock: %d, Hen: %d, Chick: %d' % (x, y, z))
