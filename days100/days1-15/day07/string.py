ed93a6293ed51b4f248d6fd1c4946796f5d8c143
jasonvan <jasondurianvan@gmail.com>
2021-01-28 23:30:56 +0800

    the 7th day of python-daily

---
+++ days100/days1-15/day07/string.py
@@ -0,0 +1,16 @@
+s1 = 'hello\t' * 3
+print(s1)
+s2 = 'world'
+s1 += s2
+print(s1)
+print('ll' in s1)
+print('good' in s1)
+
+str2 = 'abc123456'
+print(str2[2])
+print(str2[2:6])
+print(str2[2:])
+print(str2[2::2])
+print(str2[1:6:2])
+print(str2[::-1])
+print(str2[-3:-1])
