c734be29656df7670563451faab8adc6ef6cb4bd
jasonvan <jasondurianvan@gmail.com>
2021-01-29 11:26:45 +0800

    the 7th day of python-daily

---
+++ days100/days1-15/day07/string.py
@@ -0,0 +1,11 @@
+s1 = '\'hello, world!\''
+s2 = '\n\\hello, world!\\\n'
+print(s1, s2)
+
+s1 = '\141\142\143\x61\x62\x63'
+s2 = r'\u9a86\u660a'
+print(s1, s2)
+
+# print(char(1))
+# print(chr(1))
+print(0b100)
