f3053ddb2648c2274b5b7908664acaecc75d9096
jasonvan <jasondurianvan@gmail.com>
2021-02-07 17:15:26 +0800

    the 7th day of python-daily

--- days100/days1-15/day07/fibonacci.py
+++ days100/days1-15/day07/fibonacci.py
@@ -13,9 +13,25 @@ def fib(n):
         yield a
 
 
+def recursive_fib(n):
+    if n <= 1:
+        return n
+    else:
+        """
+        n = 4:
+        recursive_fib(3) + recursive_fib(2)
+        recursive_fib(2) + recursive_fib(1) + recursive_fib(1) + recursive_fib(0)
+        recursive_fib(1) + 1 + 1 + 0
+        1 + 1 + 1 + 0
+        """
+        return recursive_fib(n - 1) + recursive_fib(n - 2)
+
+
 def main():
-    for val in fib(20):
-        print(val)
+    # for val in fib(20):
+    #     print(val)
+    for val in range(1, 21):
+        print(recursive_fib(val))
 
 
 if __name__ == '__main__':
