f43c992f1b4ea31f1870f617052de373a4fb0ae3
jasonvan <jasondurianvan@gmail.com>
2021-02-19 16:56:49 +0800

    A kind of realization of Yang hui triangle

---
+++ days100/days1-15/day07/untils.py
@@ -0,0 +1,57 @@
+import random
+
+
+def generate_code(code_len=4):
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0, last_pos)
+        code += all_chars[index]
+    return code
+
+
+def get_suffix(filename, include_dot=False):
+    pos = filename.rfind('.')
+    if 0 < pos < len(filename) - 1:
+        index = pos if include_dot else pos + 1
+        return filename[index:]
+    else:
+        return ''
+
+
+def is_leap_year(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def which_day(year, month, date):
+    days_of_month = [
+        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ][is_leap_year(year)]
+    total = 0
+    for index in range(month - 1):
+        total += days_of_month[index]
+    return total + date
+
+
+def yanghui():
+    num = int(input('Number of rows: '))
+    yh = [[]] * num
+    for row in range(len(yh)):
+        yh[row] = [None] * (row + 1)
+        for col in range(len(yh[row])):
+            if col == 0 or col == row:
+                yh[row][col] = 1
+            else:
+                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
+            print(yh[row][col], end='\t')
+        print()
+
+
+if __name__ == '__main__':
+    # print(generate_code(6))
+    # print(get_suffix('hello.py', True))
+    # print(is_leap_year(2004))
+    # print(which_day(2004, 6, 14))
+    yanghui()
